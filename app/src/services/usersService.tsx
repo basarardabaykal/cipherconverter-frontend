505ea09cfa909dd896e38534fd108ffdd4e4a1c8
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-29 21:37:13 +0300

    implement usersService functions

--- app/src/services/usersService.tsx
+++ app/src/services/usersService.tsx
@@ -16,5 +16,61 @@ export const usersService = {
             toast.error("Server connection failed.")
             return null
         }
+    },
+
+    async login(data: any) {
+        try {
+            const response = await api.post(`${APP_NAME}/login/`, data)
+            if(response.data.status_message != "Success") {
+                toast.error(response.data.message)
+                return null
+            }
+            return response
+        } catch (error: any) {
+            toast.error("Server connection failed.")
+            return null
+        }
+    },
+
+    async logout(data: any) {
+        try {
+            const response = await api.post(`${APP_NAME}/logout/`, data)
+            if(response.data.status_message != "Success") {
+                toast.error(response.data.message)
+                return null
+            }
+            return response
+        } catch (error: any) {
+            toast.error("Server connection failed.")
+            return null
+        }
+    },
+
+    async refreshToken(data: any) {
+        try {
+            const response = await api.post(`${APP_NAME}/token/refresh/`, data)
+            if(response.data.status_message != "Success") {
+                toast.error(response.data.message)
+                return null
+            }
+            return response
+        } catch (error: any) {
+            toast.error("Server connection failed.")
+            return null
+        }
+    },
+
+    async me() {
+        try {
+            const response = await api.get(`${APP_NAME}/me/`)
+            if(response.data.status_message != "Success") {
+                toast.error(response.data.message)
+                return null
+            }
+            return response
+        } catch (error: any) {
+            toast.error("Server connection failed.")
+            return null
+        }
     }
 }
