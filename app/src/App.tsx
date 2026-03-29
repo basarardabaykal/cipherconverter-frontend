f828582e33c71500b5edff53264daa9d479286c9
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-03-29 18:37:51 +0300

    register new page in app.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,14 @@
 import HomePage from './pages/HomePage.tsx'
+import CaesarCipherPage from './pages/CaesarCipherPage'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return <HomePage />
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/ciphers/caesar" element={<CaesarCipherPage />} />
+    </Routes>
+  )
 }
 
 export default App
