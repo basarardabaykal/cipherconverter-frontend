eb10fb510079a969d73f0a5630497cb49ecea62f
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-01 23:07:04 +0300

    implement cipher service

---
+++ app/src/services/ciphersService.tsx
@@ -0,0 +1,34 @@
+import { api } from "./api";
+import { toast } from "react-hot-toast";
+
+const APP_NAME = "ciphers"
+
+export const ciphersService = {
+	async caesar(data: any) {
+		try {
+			const response = await api.post(`${APP_NAME}/caesar/`, data)
+			if(response.data.status_message != "Success") {
+				toast.error(response.data.message)
+				return null
+			}
+			return response
+		} catch (error: any) {
+			toast.error("Server connection failed.")
+			return null
+		}
+	},
+
+	async affine(data: any) {
+		try {
+			const response = await api.post(`${APP_NAME}/affine/`, data)
+			if(response.data.status_message != "Success") {
+				toast.error(response.data.message)
+				return null
+			}
+			return response
+		} catch (error: any) {
+			toast.error("Server connection failed.")
+			return null
+		}
+	}
+}
