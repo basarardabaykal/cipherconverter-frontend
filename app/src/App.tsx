61bb0074e9db54d77353dba4e5570a32ee35cdae
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-04 10:56:06 +0300

    add login and register pages

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,16 @@ import HomePage from './pages/HomePage.tsx'
 import CaesarCipherPage from './pages/CaesarCipherPage'
 import AffineCipherPage from './pages/AffineCipherPage.tsx'
 import TranspositionCipherPage from './pages/TranspositionCipherPage.tsx'
+import LoginPage from './pages/LoginPage.tsx'
+import RegisterPage from './pages/RegisterPage.tsx'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/ciphers/caesar" element={<CaesarCipherPage />} />
       <Route path="/ciphers/affine" element={<AffineCipherPage />} />
       <Route path="/ciphers/transposition" element={<TranspositionCipherPage />} />
