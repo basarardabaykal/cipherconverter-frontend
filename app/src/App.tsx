c7a798b097871dd4ed0c8e665e2b0cff0952f69c
Başar Arda Baykal <baykalbasararda@gmail.com>
2026-04-04 12:28:32 +0300

    mount toaster

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,18 +5,22 @@ import TranspositionCipherPage from './pages/TranspositionCipherPage.tsx'
 import LoginPage from './pages/LoginPage.tsx'
 import RegisterPage from './pages/RegisterPage.tsx'
 import { Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/ciphers/caesar" element={<CaesarCipherPage />} />
-      <Route path="/ciphers/affine" element={<AffineCipherPage />} />
-      <Route path="/ciphers/transposition" element={<TranspositionCipherPage />} />
-      <Route path="/ciphers/rail-fence" element={<TranspositionCipherPage />} />
-    </Routes>
+    <>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/ciphers/caesar" element={<CaesarCipherPage />} />
+        <Route path="/ciphers/affine" element={<AffineCipherPage />} />
+        <Route path="/ciphers/transposition" element={<TranspositionCipherPage />} />
+        <Route path="/ciphers/rail-fence" element={<TranspositionCipherPage />} />
+      </Routes>
+    </>
   )
 }
 
